968176fd65a1add46b226bc50a2b5d57b0ddfd70
hybr1dtheory <therealhybridtheory@gmail.com>
2023-10-25 23:06:22 +0300

    second commit

---
+++ homework1.py
@@ -0,0 +1,17 @@
+# Task 2
+# Hello world!
+print("Hello world!")
+
+# Beetroot!
+print("Bee", "troot", sep="", end="!\n")
+
+# Python
+# is
+# awesome!
+print("Python", "is", "awesome!", sep='\n')
+
+# Task 3
+o = "#########\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n"
+h = "#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#\n"
+print(o)
+print(h)
---
+++ homework2.py
@@ -0,0 +1,25 @@
+# Task 1
+name = "Illia"
+weekday = "Monday"
+
+print(f"Good day, {name}! {weekday} is a perfect day to learn python.")
+print("Hello, {}! It is {} today.".format(name, weekday), end="\n\n")
+
+# Task 2
+name = "Illia"
+surname = "Halchenko"
+
+print("Welcome to python, " + name + " " + surname, end="\n\n")
+
+# Task 3
+a = 8
+b = 5
+
+print(f"a + b = {a+b}")
+print(f"a - b = {a-b}")
+print(f"a / b = {a/b}")
+print(f"a * b = {a*b}")
+print(f"a^b = {a**b}")
+print(f"a % b = {a%b}")
+print(f"a // b = {a//b}")
+
