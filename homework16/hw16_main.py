43494d98db601d1e6c8b64dfbe780c1ad45fe6de
hybr1dtheory <therealhybridtheory@gmail.com>
2023-12-06 01:08:42 +0200

    task3 and main updated

--- homework16/hw16_main.py
+++ homework16/hw16_main.py
@@ -1,5 +1,7 @@
 from task1 import Teacher, Student
 from task2 import Mathematician
+from task3 import ProductStore, Product
+from task4 import NotInRangeError, test_except
 
 
 # Task 1 testing
@@ -19,3 +21,23 @@ m = Mathematician()
 print(m.square_nums([7, 11, 5, 4]))
 print(m.remove_positives([26, -11, -8, 13, -90]))
 print(m.filter_leaps([2001, 1884, 1995, 2003, 2020, 2100]))
+
+# Task 3 testing
+print("\n\nTask 3\n")
+p = Product('Sport', 'Football T-Shirt', 100)
+p2 = Product('Food', 'Ramen', 1.5)
+s = ProductStore()
+s.add(p, 10)
+s.add(p2, 300)
+print(*s.get_all_products(), sep='')
+print(s.get_income())
+s.sell_product('Ramen', 10)
+print(s.get_product_info('Ramen'))
+s.set_discount("Football T-Shirt", 50)
+print(*s.get_all_products(), sep='')
+
+# Task 4 testing
+try:
+    print(test_except(1, 10, 100))
+except NotInRangeError as e:
+    print(e)
--- homework16/task3.py
+++ homework16/task3.py
@@ -22,8 +22,9 @@ class ProductStore:
     def add(self, product: Product, amount: int):
         if not isinstance(product, Product):
             raise ValueError("The product argument must be a Product type")
-        product.price = product.price * 1.3
-        self._prod_list.append(product.add(amount))
+        product.price = round(product.price * 1.3, 2)
+        product.add(amount)
+        self._prod_list.append(product)
 
     def set_discount(self, identifier: str, percent: float, identifier_type="name"):
         if identifier_type not in ('name', 'type'):
@@ -48,3 +49,25 @@ class ProductStore:
                     break
         else:
             print(f"The product with name {prod_name} was not found")
+
+    def get_income(self):
+        total = [p.amount for p in self._prod_list]
+        return sum(total)
+
+    def get_all_products(self) -> list:
+        """Return a list of str. Every str represents info about one product."""
+        info = []
+        i = 1
+        for p in self._prod_list:
+            s = f"{i}. Product name: {p.name} \tType: {p.type} \tAmount: {p.amount} \tPrice: {p.price}\n"
+            info.append(s)
+            i += 1
+        return info
+
+    def get_product_info(self, prod_name: str) -> tuple:
+        """Returns a tuple with product name and amount. If name was not found returns empty tuple."""
+        for prod in self._prod_list:
+            if prod_name == prod.name:
+                return prod.name, prod.amount
+        else:
+            return ()
