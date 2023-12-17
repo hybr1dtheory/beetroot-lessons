323357d73a4de642db4059f31d47859af793419c
hybr1dtheory <therealhybridtheory@gmail.com>
2023-12-17 15:50:19 +0200

    update for task 2

--- homework18/hw18_main.py
+++ homework18/hw18_main.py
@@ -1,5 +1,6 @@
 from task3 import TypeDecorators
 from task1 import User
+from task2 import Worker, Boss
 
 
 # Task 3 testing
@@ -28,6 +29,7 @@ assert do_something('True') is True
 assert add(5, 7) == '12'
 assert get_str_float('2', '055') == 2.055
 
+
 # Task 1 testing
 # Valid emails
 assert User.validate('user@example.com')
@@ -40,3 +42,24 @@ print(User.validate('abc.example.com'))
 print(User.validate('a@b@c@example.com'))
 print(User.validate('i.like.underscores@but_they_are_not_allowed_in_this_part.com'))
 print(User.validate('abc@example.222'))
+
+
+# Task 2 testing
+b1 = Boss(1, "First Boss", "Beetroot")
+w1 = Worker(1, "John Smith", "Beetroot", b1)
+w2 = Worker(2, "Sam Adams", "Beetroot", b1)
+w3 = Worker(3, "James Black", "Beetroot", b1)
+b1.add_worker(w1, w2, w3)
+print(*b1.get_workers(), sep='\n')
+b2 = Boss(2, "Second Boss", "Another company")
+print(w3.boss.name)
+w3.boss = b2
+print(w3.boss.name)
+try:
+    w3.boss = "another boss"
+except ValueError:
+    print("Only Boss type can be added to boss attribute")
+try:
+    b2.add_worker("some worker")
+except ValueError:
+    print("Only Worker type can be added to workers list")
--- homework18/task2.py
+++ homework18/task2.py
@@ -1,16 +1,40 @@
 class Boss:
-
+    """Represents a manager who has a certain number of subordinate workers"""
     def __init__(self, id_: int, name: str, company: str):
         self.id = id_
         self.name = name
         self.company = company
-        self.workers = []
+        self.__workers = []
+
+    def get_workers(self):
+        return self.__workers.copy()
+
+    def add_worker(self, *workers):
+        for worker in workers:
+            if isinstance(worker, Worker):
+                self.__workers.append(worker)
+            else:
+                raise ValueError("Only instance of the class Worker can be added to workers list")
 
 
 class Worker:
-
+    """Represents an employee who is subordinated to a line manager"""
     def __init__(self, id_: int, name: str, company: str, boss: Boss):
         self.id = id_
         self.name = name
         self.company = company
         self.boss = boss
+
+    def __str__(self):
+        return f"ID: {self.id}, Name: {self.name}, Company: {self.company}, Boss: {self.boss.name}"
+
+    @property
+    def boss(self):
+        return self.__boss
+
+    @boss.setter
+    def boss(self, boss):
+        if isinstance(boss, Boss):
+            self.__boss = boss
+        else:
+            raise ValueError('Attribute boss must be an instance of the class Boss')
